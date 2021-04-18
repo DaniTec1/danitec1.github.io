69d5ae71daaddff62468500bf435dcb66f45f4ad
Dani-Rosas753 <64808943+Dani-Rosas753@users.noreply.github.com>
2021-04-17 22:01:55 -0500

    SitioWeb

---
+++ precache-manifest.d606402444e85d07aa79062894d19de6.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8a023d0b1ee25666e1f",
+    "url": "/css/app.60ffd151.css"
+  },
+  {
+    "revision": "2ec9e87f4add7f22f9537354ddab1e78",
+    "url": "/img/Banner1.2ec9e87f.png"
+  },
+  {
+    "revision": "883deb785a4b1e0063985e233e749a7a",
+    "url": "/img/Banner2.883deb78.png"
+  },
+  {
+    "revision": "2762990977d090118844797487333deb",
+    "url": "/img/Banner3.27629909.png"
+  },
+  {
+    "revision": "d2c1f5539510192ace3873ff83b4f623",
+    "url": "/img/clear.d2c1f553.svg"
+  },
+  {
+    "revision": "d5ee9ea2df4cc398f568f9f68a7231dc",
+    "url": "/img/diag.d5ee9ea2.svg"
+  },
+  {
+    "revision": "55ec65219574f39d70a1e5318b7e56ee",
+    "url": "/img/soft.55ec6521.svg"
+  },
+  {
+    "revision": "847607f2477591b90b3628a50b0e443f",
+    "url": "/img/soport.847607f2.svg"
+  },
+  {
+    "revision": "c1f3f39b9086a9fd9d21284e86d05358",
+    "url": "/img/speed.c1f3f39b.svg"
+  },
+  {
+    "revision": "43019532159593a20458d239f0b38eee",
+    "url": "/img/tools.43019532.svg"
+  },
+  {
+    "revision": "e0c2ee0bf5e8d16e311bdbbb6b69d43a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8a023d0b1ee25666e1f",
+    "url": "/js/app.ccc1ac36.js"
+  },
+  {
+    "revision": "7d294b4df7196f51592d",
+    "url": "/js/chunk-vendors.7b990b82.js"
+  },
+  {
+    "revision": "8442dded7e1488c9d051a4839316c730",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
