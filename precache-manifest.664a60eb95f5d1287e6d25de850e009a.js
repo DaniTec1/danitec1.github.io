614ccefa0fe3bbef021f348c71350cbdfb223348
Dani-Rosas753 <64808943+Dani-Rosas753@users.noreply.github.com>
2021-04-25 14:00:53 -0500

    Solucion Modal

---
+++ precache-manifest.664a60eb95f5d1287e6d25de850e009a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb9f37c7edd6fb632e1c",
+    "url": "/css/app.cac456df.css"
+  },
+  {
+    "revision": "2ec9e87f4add7f22f9537354ddab1e78",
+    "url": "/img/Banner1.2ec9e87f.png"
+  },
+  {
+    "revision": "883deb785a4b1e0063985e233e749a7a",
+    "url": "/img/Banner2.883deb78.png"
+  },
+  {
+    "revision": "2762990977d090118844797487333deb",
+    "url": "/img/Banner3.27629909.png"
+  },
+  {
+    "revision": "d2c1f5539510192ace3873ff83b4f623",
+    "url": "/img/clear.d2c1f553.svg"
+  },
+  {
+    "revision": "d5ee9ea2df4cc398f568f9f68a7231dc",
+    "url": "/img/diag.d5ee9ea2.svg"
+  },
+  {
+    "revision": "55ec65219574f39d70a1e5318b7e56ee",
+    "url": "/img/soft.55ec6521.svg"
+  },
+  {
+    "revision": "847607f2477591b90b3628a50b0e443f",
+    "url": "/img/soport.847607f2.svg"
+  },
+  {
+    "revision": "c1f3f39b9086a9fd9d21284e86d05358",
+    "url": "/img/speed.c1f3f39b.svg"
+  },
+  {
+    "revision": "43019532159593a20458d239f0b38eee",
+    "url": "/img/tools.43019532.svg"
+  },
+  {
+    "revision": "ee3ba4b6091ef5b3a1bc7b81e3106a1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb9f37c7edd6fb632e1c",
+    "url": "/js/app.4ff5f77f.js"
+  },
+  {
+    "revision": "32567eab72c8660baa7f",
+    "url": "/js/chunk-vendors.2c6fdebd.js"
+  },
+  {
+    "revision": "3f0efb3d6ba8ce28ea9043d6103937dc",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
