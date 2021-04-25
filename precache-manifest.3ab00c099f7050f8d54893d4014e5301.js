972a46622cf61800a66680472e80927b4cdc6ca7
Dani-Rosas753 <64808943+Dani-Rosas753@users.noreply.github.com>
2021-04-25 03:22:09 -0500

    Error solucionado de iconos

---
+++ precache-manifest.3ab00c099f7050f8d54893d4014e5301.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6054028b2b7c0fcc75e2",
+    "url": "/css/app.cac456df.css"
+  },
+  {
+    "revision": "2ec9e87f4add7f22f9537354ddab1e78",
+    "url": "/img/Banner1.2ec9e87f.png"
+  },
+  {
+    "revision": "883deb785a4b1e0063985e233e749a7a",
+    "url": "/img/Banner2.883deb78.png"
+  },
+  {
+    "revision": "2762990977d090118844797487333deb",
+    "url": "/img/Banner3.27629909.png"
+  },
+  {
+    "revision": "d2c1f5539510192ace3873ff83b4f623",
+    "url": "/img/clear.d2c1f553.svg"
+  },
+  {
+    "revision": "d5ee9ea2df4cc398f568f9f68a7231dc",
+    "url": "/img/diag.d5ee9ea2.svg"
+  },
+  {
+    "revision": "55ec65219574f39d70a1e5318b7e56ee",
+    "url": "/img/soft.55ec6521.svg"
+  },
+  {
+    "revision": "847607f2477591b90b3628a50b0e443f",
+    "url": "/img/soport.847607f2.svg"
+  },
+  {
+    "revision": "c1f3f39b9086a9fd9d21284e86d05358",
+    "url": "/img/speed.c1f3f39b.svg"
+  },
+  {
+    "revision": "43019532159593a20458d239f0b38eee",
+    "url": "/img/tools.43019532.svg"
+  },
+  {
+    "revision": "a89e17d5e4aaaf2bc3e5f92f5b868b42",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6054028b2b7c0fcc75e2",
+    "url": "/js/app.eadf8743.js"
+  },
+  {
+    "revision": "32567eab72c8660baa7f",
+    "url": "/js/chunk-vendors.2c6fdebd.js"
+  },
+  {
+    "revision": "4414cd1f6784a95ef731da84a573df1e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
